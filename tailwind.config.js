cdcd7babc2d7012e847ee39ecf5886134adea96c
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-05 15:52:26 +0600

    Added redux and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+	purge: { enabled: true, content: ['./src/**/*.{js,jsx,ts,tsx,html}'] },
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
