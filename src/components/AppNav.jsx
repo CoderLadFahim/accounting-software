1fcbb1195401124a1f47a6f54e04bbb2bae4c52e
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-07 10:50:50 +0600

    Started adding AppNav markup

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,17 @@
+function AppNav() {
+	return (
+		<nav>
+			<div className="container">
+				<div className="user-profile">
+					<img src="" alt="" />
+
+					<div className="user-data">
+						<h2 className="user-name">Classic IT</h2>
+
+						<h3 className="user-role">Admin</h3>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
