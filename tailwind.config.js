1b8c94ebf276a19d7459a2466af2bbe68e70114d
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-07 10:42:47 +0600

    Configured the tailwindcss container class to be centered by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
 	purge: { enabled: true, content: ['./src/**/*.{js,jsx,ts,tsx,html}'] },
 	theme: {
+		container: {
+			center: true,
+		},
 		extend: {},
 	},
 	plugins: [],
