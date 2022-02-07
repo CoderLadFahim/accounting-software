9f2a1d2be79d1a60ba2ddb3c893d97c628e9907a
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-07 16:24:30 +0600

    Imported the roboto font and different weights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import AppNav from './components/AppNav';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<header className="App-header">
+				<AppNav />
+			</header>
 		</div>
 	);
 }
