e42e92c42746dbf661519af6dfcb3537c1826e21
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-08 11:26:46 +0600

    Moved the AppNav.jsx component to an AppNav/ folder and added an AppNavStyle.css file to store reusable tailwind classes

---
+++ src/components/AppNav/AppNav.jsx
@@ -0,0 +1,35 @@
+import './AppNavStyles.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalculator, faBell } from '@fortawesome/free-solid-svg-icons';
+
+function AppNav() {
+	return (
+		<nav class="fixed w-screen top-0 h-16 shadow bg-white text-gray-700 grid place-items-center">
+			<div className="container border flex justify-between items-center">
+				<div className="user-profile flex items-center text-left space-x-3">
+					<img src="logos_temp/Logo.png" alt="User logo" />
+
+					<div className="user-data flex flex-col items-start">
+						<span className="user-name">Classic IT</span>
+<br />
+
+						<span className="user-role text-red-600 text-sm">Admin</span>
+					</div>
+				</div>
+
+				<div className="other-btns space-x-5">
+					<button class="btn">
+						<FontAwesomeIcon icon={faCalculator} />
+					</button>
+					<button class="btn text-white bg-gray-700 relative">
+						<FontAwesomeIcon icon={faBell} />
+						<div className="active h-2 w-2 rounded-full bg-red-400 absolute top-1 right-0"></div>
+					</button>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default AppNav;
