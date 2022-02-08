c018c919d44d60227fa72866a5dd3d400b1ca562
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-08 18:59:58 +0600

    surrounded the nav icon and the text in a div.nav-link-content

--- src/components/SideNav/DropdownMenu.jsx
+++ src/components/SideNav/DropdownMenu.jsx
@@ -20,13 +20,17 @@ function DropdownMenu() {
 	return (
 		<ul className="nav-links">
 			<li className="nav-link active">
-				<FontAwesomeIcon icon={faHome} className="nav-icon" />
-				Home
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faHome} className="nav-icon" />
+					<span>Home</span>
+				</div>
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faAddressBook} className="nav-icon" />
-				Contact
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faAddressBook} className="nav-icon" />
+					<span>Contact</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 				<ul className="hidden">
 					<li>Customers</li>
@@ -35,8 +39,10 @@ function DropdownMenu() {
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faBoxes} className="nav-icon" />
-				Products
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faBoxes} className="nav-icon" />
+					<span>Products</span>
+				</div>
 				<ul className="hidden">
 					<li>Product List</li>
 					<li>Categories</li>
@@ -49,49 +55,65 @@ function DropdownMenu() {
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faShoppingCart} className="nav-icon" />
-				Sales
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faShoppingCart} className="nav-icon" />
+					<span>Sales</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faMoneyCheck} className="nav-icon" />
-				Purchases
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faMoneyCheck} className="nav-icon" />
+					<span>Purchases</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faMoneyBillWave} className="nav-icon" />
-				Payment
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faMoneyBillWave} className="nav-icon" />
+					<span>Payment</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faWallet} className="nav-icon" />
-				Expense
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faWallet} className="nav-icon" />
+					<span>Expense</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faPiggyBank} className="nav-icon" />
-				Banks
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faPiggyBank} className="nav-icon" />
+					<span>Banks</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 
 			<li className="nav-link">
-				Language
-				<FontAwesomeIcon icon={faLanguage} className="nav-icon" />
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faLanguage} className="nav-icon" />
+					<span>Language</span>
+				</div>
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faUser} className="nav-icon" />
-				Users
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faUser} className="nav-icon" />
+					<span>Users</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 
 			<li className="nav-link">
-				<FontAwesomeIcon icon={faFileAlt} className="nav-icon" />
-				Reports
+				<div className="nav-link-content space-x-3">
+					<FontAwesomeIcon icon={faFileAlt} className="nav-icon" />
+					<span>Reports</span>
+				</div>
 				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
 			</li>
 		</ul>
