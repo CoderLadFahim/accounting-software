4851cb53065d2d7a1b56799675cb947e4f3537c9
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-08 11:36:44 +0600

    Made the notification dot dynamic and dependant on a notificationExists state variable which is set to true but can be set to false on notification btn click

--- src/components/AppNav/AppNav.jsx
+++ src/components/AppNav/AppNav.jsx
@@ -1,11 +1,14 @@
 import './AppNavStyles.css';
 
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalculator, faBell } from '@fortawesome/free-solid-svg-icons';
 
 function AppNav() {
+	const [notificationExists, setNotificationExists] = useState(true);
+
 	return (
-		<nav class="fixed w-screen top-0 h-16 shadow bg-white text-gray-700 grid place-items-center">
+		<nav className="fixed w-screen top-0 h-16 shadow bg-white text-gray-700 grid place-items-center">
 			<div className="container flex justify-between items-center">
 				<div className="user-profile flex items-center text-left space-x-3">
 					<img src="logos_temp/Logo.png" alt="User logo" />
@@ -19,12 +22,17 @@ function AppNav() {
 				</div>
 
 				<div className="other-btns space-x-5">
-					<button class="btn">
+					<button className="btn">
 						<FontAwesomeIcon icon={faCalculator} />
 					</button>
-					<button class="btn text-white bg-gray-700 relative">
+					<button
+						className="btn text-white bg-gray-700 relative"
+						onClick={() => setNotificationExists(false)}
+					>
 						<FontAwesomeIcon icon={faBell} />
-						<div className="active h-2 w-2 rounded-full bg-red-400 absolute top-1 right-0"></div>
+						{notificationExists && (
+							<div className="active h-2 w-2 rounded-full bg-red-400 absolute top-1 right-0"></div>
+						)}
 					</button>
 				</div>
 			</div>
