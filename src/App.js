7c12f45d5fc56d04a2af9204b971f79ce323fc1d
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-08 11:11:21 +0600

    started adding nav classes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AppNav from './components/AppNav';
 
 function App() {
 	return (
-		<div className="App bg-gray-50">
+		<div className="App bg-gray-50 font-roboto">
 			<header className="App-header">
 				<AppNav />
 			</header>
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -3,15 +3,15 @@ import { faCalculator, faBell } from '@fortawesome/free-solid-svg-icons';
 
 function AppNav() {
 	return (
-		<nav>
-			<div className="container">
-				<div className="user-profile">
+		<nav class="fixed w-screen top-0 h-16 shadow bg-white">
+			<div className="container border flex justify-between">
+				<div className="user-profile text-gray-700">
 					<img src="logos_temp/Logo.png" alt="User logo" />
 
 					<div className="user-data">
 						<h2 className="user-name">Classic IT</h2>
 
-						<h3 className="user-role">Admin</h3>
+						<h3 className="user-role text-red-600">Admin</h3>
 					</div>
 				</div>
 
