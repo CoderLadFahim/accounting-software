be24bc5bffc9343ec3188a6f2f80e0c70c228e3e
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-08 17:58:22 +0600

    Imported the necessary icons for dropdown menu

--- src/components/SideNav/DropdownMenu.jsx
+++ src/components/SideNav/DropdownMenu.jsx
@@ -1,28 +1,43 @@
 import './DropdownMenuStyles.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faHome, faAddressBook } from '@fortawesome/free-solid-svg-icons';
+import {
+	faCaretDown,
+	faHome,
+	faAddressBook,
+	faBoxes,
+	faShoppingCart,
+	faMoneyCheck,
+	faMoneyBillWave,
+	faWallet,
+	faPiggyBank,
+	faUser,
+	faLanguage,
+	faFileAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 function DropdownMenu() {
 	return (
 		<ul className="nav-links">
-			<li className="nav-link">
-				<FontAwesomeIcon icon={ faHome } />
-			Home</li>
+			<li className="nav-link active">
+				<FontAwesomeIcon icon={faHome} />
+				Home
+			</li>
 
 			<li className="nav-link">
 				<FontAwesomeIcon icon={faAddressBook} />
 				Contact
 				<FontAwesomeIcon icon={faCaretDown} />
-				<ul>
+				<ul className="hidden">
 					<li>Customers</li>
 					<li>Suppliers</li>
 				</ul>
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faBoxes} />
 				Products
-				<ul>
+				<ul className="hidden">
 					<li>Product List</li>
 					<li>Categories</li>
 					<li>Variations</li>
@@ -30,46 +45,52 @@ function DropdownMenu() {
 					<li>Brands</li>
 					<li>Warranties</li>
 				</ul>
-
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faShoppingCart} />
 				Sales
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faMoneyCheck} />
 				Purchases
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faMoneyBillWave} />
 				Payment
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faWallet} />
 				Expense
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faPiggyBank} />
 				Banks
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
 				Language
-				<FontAwesomeIcon icon={faCaretDown} />
+				<FontAwesomeIcon icon={faLanguage} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faUser} />
 				Users
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
 
 			<li className="nav-link">
+				<FontAwesomeIcon icon={faFileAlt} />
 				Reports
 				<FontAwesomeIcon icon={faCaretDown} />
 			</li>
