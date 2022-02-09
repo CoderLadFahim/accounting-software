60dafd4bf7fff8de044f10950c456ba76a1440c2
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-09 11:33:25 +0600

    Moved some tailwind  classes to NavLinkjsx from NavLinkStylecss

--- src/components/SideNav/NavLink.jsx
+++ src/components/SideNav/NavLink.jsx
@@ -4,15 +4,18 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 function NavLink({ children, icon, spanText }) {
 	return (
-		<li className="nav-link">
+		<li className="nav-link bg-gray-800 py-2 text-left text-base relative hover:bg-gray-700 cursor-pointer pl-4 last:border-b last:border-teal-300">
 			<div className="nav-link-content space-x-3">
 				{icon}
-				<span>{spanText}</span>
+				<span className="hover:text-teal-200">{spanText}</span>
 			</div>
 
 			{children}
 			{children && (
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
+				<FontAwesomeIcon
+					icon={faCaretDown}
+					className="caret-down absolute right-4 top-3"
+				/>
 			)}
 		</li>
 	);
