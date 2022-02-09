1a185fe29b16f74ed84ed7afca08e7f7ec7b9417
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-09 11:23:49 +0600

    Reusing the NavLink component in DropDown

--- src/components/SideNav/DropdownMenu.jsx
+++ src/components/SideNav/DropdownMenu.jsx
@@ -3,7 +3,6 @@ import './DropdownMenuStyles.css';
 import NavLink from './NavLink.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-	faCaretDown,
 	faHome,
 	faAddressBook,
 	faBoxes,
@@ -20,29 +19,26 @@ import {
 function DropdownMenu() {
 	return (
 		<ul className="nav-links">
-			<NavLink icon={<FontAwesomeIcon icon={faHome} className="nav-icon" />} spanText="Home"/>
+			<NavLink
+				icon={<FontAwesomeIcon icon={faHome} className="nav-icon" />}
+				spanText="Home"
+			/>
 
-			<NavLink icon={<FontAwesomeIcon icon={faAddressBook} className="nav-icon" />} spanText="Contact">
+			<NavLink
+				icon={<FontAwesomeIcon icon={faAddressBook} className="nav-icon" />}
+				spanText="Contact"
+			>
 				<ul>
 					<li>Customers</li>
 					<li>Suppliers</li>
+					<li>Import Contacts</li>
 				</ul>
 			</NavLink>
 
-			{/* <li className="nav-link"> */}
-			{/* 	<div className="nav-link-content space-x-3"> */}
-			{/* 		<FontAwesomeIcon icon={faAddressBook} className="nav-icon" /> */}
-			{/* 		<span>Contact</span> */}
-			{/* 	</div> */}
-			{/* 	<FontAwesomeIcon icon={faCaretDown} className="caret-down" /> */}
-				
-			{/* </li> */}
-
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faBoxes} className="nav-icon" />
-					<span>Products</span>
-				</div>
+			<NavLink
+				icon={<FontAwesomeIcon icon={faBoxes} className="nav-icon" />}
+				spanText="Products"
+			>
 				<ul>
 					<li>Product List</li>
 					<li>Categories</li>
@@ -51,71 +47,94 @@ function DropdownMenu() {
 					<li>Brands</li>
 					<li>Warranties</li>
 				</ul>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
+			<NavLink
+				icon={
 					<FontAwesomeIcon icon={faShoppingCart} className="nav-icon" />
-					<span>Sales</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+				}
+				spanText="Sales"
+			>
+				<ul>
+					<li>Sales List</li>
+					<li>Add Sale</li>
+					<li>POS List</li>
+					<li>POS</li>
+					<li>Drafts</li>
+					<li>Add Drafts</li>
+					<li>Quotations</li>
+					<li>Add Quotation</li>
+					<li>Returned Sales</li>
+					<li>Shipments</li>
+					<li>Discounts</li>
+					<li>Import Sales</li>
+				</ul>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faMoneyCheck} className="nav-icon" />
-					<span>Purchases</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+			<NavLink
+				icon={<FontAwesomeIcon icon={faMoneyCheck} className="nav-icon" />}
+				spanText="Purchases"
+			>
+				<ul>
+					<li>Purchases List</li>
+					<li>Add Purchase</li>
+					<li>Returned purchases</li>
+				</ul>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
+			<NavLink
+				className="nav-link"
+				icon={
 					<FontAwesomeIcon icon={faMoneyBillWave} className="nav-icon" />
-					<span>Payment</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+				}
+				spanText="Payment"
+			>
+				<ul>
+					<li>Received</li>
+					<li>Paid</li>
+					<li>Add Payment</li>
+				</ul>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faWallet} className="nav-icon" />
-					<span>Expense</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+			<NavLink
+				icon={<FontAwesomeIcon icon={faWallet} className="nav-icon" />}
+				spanText="Expenses"
+			>
+				<ul>
+					<li>Expense List</li>
+					<li>Add Expense</li>
+					<li>Expense Categories</li>
+				</ul>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faPiggyBank} className="nav-icon" />
-					<span>Banks</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+			<NavLink icon={<FontAwesomeIcon icon={faPiggyBank} className="nav-icon" />} spanText="Banks">
+				<ul>
+					<li>Manage Accounts</li>
+					<li>Add Accounts</li>
+				</ul>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faLanguage} className="nav-icon" />
-					<span>Language</span>
-				</div>
-			</li>
+			<NavLink icon={<FontAwesomeIcon icon={faLanguage} className="nav-icon" />} spanText="Language">
+				<ul>
+					<li>English</li>
+					<li>Bangla</li>
+				</ul>
+			</NavLink>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faUser} className="nav-icon" />
-					<span>Users</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
 
-			<li className="nav-link">
-				<div className="nav-link-content space-x-3">
-					<FontAwesomeIcon icon={faFileAlt} className="nav-icon" />
-					<span>Reports</span>
-				</div>
-				<FontAwesomeIcon icon={faCaretDown} className="caret-down" />
-			</li>
+			<NavLink icon={<FontAwesomeIcon icon={faUser} className="nav-icon" />} spanText="Users">
+				<ul>
+					<li>Users</li>
+					<li>Roles</li>
+				</ul>
+			</NavLink>
+			<NavLink icon={<FontAwesomeIcon icon={faFileAlt} className="nav-icon" />} spanText="Reports">
+				<ul>
+					<li>Sales Reports</li>
+					<li>Purchase Reports</li>
+				</ul>
+			</NavLink>
+
 		</ul>
 	);
 }
