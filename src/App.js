1264bd5730f573b19e0d96c8a41c83b308d1814f
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-14 12:51:55 +0600

    tabular data being properly displayed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,28 @@ function App() {
 		<div className="App bg-gray-50 font-roboto">
 			<header className="App-header">
 				<AppNav />
-				<Table />
+				<Table data={
+					[
+						{
+							serial: 0,
+								date: '14-2-2002',
+								name: 'fahim',
+								balance: 10000,
+					},
+						{
+							serial: 1,
+								date: '13-2-2002',
+								name: 'Nazmul',
+								balance: 14000,
+						},
+						{
+							serial: 2,
+							date: '16-2-2002',
+							name: 'Nishat',
+							balance: 12000,
+						},
+					]
+				}/>
 			</header>
 		</div>
 	);
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,55 +1,27 @@
 import './TableStyles.css';
-import { useEffect } from 'react';
 
-function Table(/*{ data }*/) {
-	const data = [
-		{
-			serial: 0,
-			date: '14-2-2002',
-			name: 'fahim',
-			balance: 10000,
-		},
-		{
-			serial: 1,
-			date: '13-2-2002',
-			name: 'Nazmul',
-			balance: 14000,
-		},
-		{
-			serial: 2,
-			date: '16-2-2002',
-			name: 'Nishat',
-			balance: 12000,
-		},
-	];
-
+function Table({ data }) {
 	const columns = Array.from(new Set(Object.keys(data[0])));
-	const values = data.map(v => Object.values(v));
-
-	useEffect(() => {
-		console.log(values);
-	}, []);
+	const values = data.map((v) => Object.values(v));
 
 	return (
 		<table className="app-table">
 			<thead>
 				<tr>
-					{columns.map(el => <th className="capitalize">{el}</th>)}
+					{columns.map((column) => (
+						<th className="capitalize">{column}</th>
+					))}
 				</tr>
 			</thead>
+
 			<tbody>
-				<tr>
-					<td>0</td>
-					<td>14-2-2002</td>
-					<td>Fahim</td>
-					<td>10000</td>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td>13-2-2002</td>
-					<td>Nazmul</td>
-					<td>14000</td>
-				</tr>
+				{values.map((row) => (
+					<tr className="capitalize" >
+						{row.map((property) => (
+							<td>{property}</td>
+						))}
+					</tr>
+				))}
 			</tbody>
 		</table>
 	);
