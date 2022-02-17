eaa91a9cf7ee600016ae51f086c2b17f39147ee5
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-17 16:16:18 +0600

    removed img tag from LoginForm

--- src/views/LoginForm/LoginForm.jsx
+++ src/views/LoginForm/LoginForm.jsx
@@ -5,30 +5,28 @@ import AppButton from '../../components/AppButton.jsx';
 function LoginForm() {
 	return (
 		<section className="LoginForm h-screen w-screen border border-pink-400">
-			<div className="app-form-parent">
-				<div className="heading">
-					<div className="company-branding">
-						<img src="" alt="" />
-						<h3>COMPANY_NAME</h3>
-					</div>
+			 <div className="app-form-parent"> 
+			 	<div className="heading"> 
+			 		<div className="company-branding"> 
+			 			<img src="" alt="" /> 
+			 			<h3>COMPANY_NAME</h3> 
+			 		</div> 
 
-					<h2>Login to your account</h2>
-				</div>
+			 		<h2>Login to your account</h2> 
+			 	</div> 
 
-				<form action="">
-					<AppInput labelText="Username" />
-					<AppInput labelText="Password" />
+			 	<form action=""> 
+			 		<AppInput labelText="Username" /> 
+			 		<AppInput labelText="Password" /> 
 
-					<AppButton
-						className="rounded-md"
-						onClick={() => console.log('heelo')}
-					>
-						Login
-					</AppButton>
-				</form>
-			</div>
-
-			<img src="login-bg.jpeg" alt="Form img" />
+			 		<AppButton 
+			 			className="rounded-md" 
+			 			onClick={() => console.log('heelo')} 
+			 		> 
+			 			Login 
+			 		</AppButton> 
+			 	</form> 
+			 </div> 
 		</section>
 	);
 }
