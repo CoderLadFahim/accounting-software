9beaa2e9af4aa8ed1d3acf4029148a0c457dd687
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-17 17:39:10 +0600

    Removed excessive classes from AppButton

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -3,7 +3,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const AppButton = ({ children, className, ...props }) => (
 	<button
-		className={`AppButton absolute transform top-1/2 left-1/2 bg-teal-600 px-4 transform transition hover:bg-teal-500
+		className={`AppButton bg-teal-600 px-6 transform transition hover:bg-teal-500
 		py-2 rounded-2xl drop-shadow-md text-white flex items-center justify-between
 		gap-4
 		${className}`}
--- src/views/LoginForm/LoginForm.jsx
+++ src/views/LoginForm/LoginForm.jsx
@@ -4,29 +4,37 @@ import AppButton from '../../components/AppButton.jsx';
 
 function LoginForm() {
 	return (
-		<section className="LoginForm h-screen w-screen border border-pink-400">
-			 <div className="app-form-parent"> 
-			 	<div className="heading"> 
-			 		<div className="company-branding"> 
-			 			<img src="" alt="" /> 
-			 			<h3>COMPANY_NAME</h3> 
-			 		</div> 
+		<section className="LoginForm h-screen w-screen bg-sky-500">
+			<div className="app-form-parent bg-white flex flex-col items-center justify-evenly w-9/12 h-64 mx-auto rounded-xl p-8">
+				<div className="heading">
+					<div className="company-branding flex items-center justify-center gap-3">
+						<img
+							src="logos_temp/Logo.png"
+							alt="logo"
+							className="w-7 h-7"
+						/>
+						<h3 className="text-gray-800 text-lg">
+							Classic IT & Sky Mart Ltd.
+						</h3>
+					</div>
 
-			 		<h2>Login to your account</h2> 
-			 	</div> 
+					<h2 className="text-gray-700">Login to your account</h2>
+				</div>
 
-			 	<form action=""> 
-			 		<AppInput labelText="Username" /> 
-			 		<AppInput labelText="Password" /> 
+				<form action="" className="w-full flex flex-col items-center">
+					<AppInput labelText="Username" />
+					<AppInput labelText="Password" />
 
-			 		<AppButton 
-			 			className="rounded-md" 
-			 			onClick={() => console.log('heelo')} 
-			 		> 
-			 			Login 
-			 		</AppButton> 
-			 	</form> 
-			 </div> 
+					<AppButton
+						className="rounded-md bg-blue-500"
+						onClick={() => console.log('heelo')}
+					>
+						<span className="text-base font-bold hover:bg-blue-400">
+							Login
+						</span>
+					</AppButton>
+				</form>
+			</div>
 		</section>
 	);
 }
