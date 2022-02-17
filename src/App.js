8db69c2bc6c9a36e0ac05aad04b8448ee2c8bc1b
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-17 14:41:19 +0600

    Added LoginForm markup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import LoginForm from './views/LoginForm/LoginForm.jsx';
 import AppNav from './components/AppNav/AppNav.jsx';
 import SideBar from './components/SideBar/SideBar.jsx';
 import AppInput from './components/AppInput/AppInput.jsx';
@@ -9,31 +10,32 @@ import HamburgerMenu from './components/HamburgerMenu.jsx';
 function App() {
 	return (
 		<div className="App bg-gray-50 font-roboto">
-			<header className="App-header">
-				<AppNav />
-				<Table
-					data={[
-						{
-							serial: 0,
-							date: '14-2-2002',
-							name: 'fahim',
-							balance: 10000,
-						},
-						{
-							serial: 1,
-							date: '13-2-2002',
-							name: 'Nazmul',
-							balance: 14000,
-						},
-						{
-							serial: 2,
-							date: '16-2-2002',
-							name: 'Nishat',
-							balance: 12000,
-						},
-					]}
-				/>
-			</header>
+			<LoginForm />
+			{/* <header className="Aap-header"> */}
+			{/* 	<AppNav /> */}
+			{/* 	<Table */}
+			{/* 		data={[ */}
+			{/* 			{ */}
+			{/* 				serial: 0, */}
+			{/* 				date: '14-2-2002', */}
+			{/* 				name: 'fahim', */}
+			{/* 				balance: 10000, */}
+			{/* 			}, */}
+			{/* 			{ */}
+			{/* 				serial: 1, */}
+			{/* 				date: '13-2-2002', */}
+			{/* 				name: 'Nazmul', */}
+			{/* 				balance: 14000, */}
+			{/* 			}, */}
+			{/* 			{ */}
+			{/* 				serial: 2, */}
+			{/* 				date: '16-2-2002', */}
+			{/* 				name: 'Nishat', */}
+			{/* 				balance: 12000, */}
+			{/* 			}, */}
+			{/* 		]} */}
+			{/* 	/> */}
+			{/* </header> */}
 		</div>
 	);
 }
--- src/views/LoginForm/LoginForm.jsx
+++ src/views/LoginForm/LoginForm.jsx
@@ -1,7 +1,31 @@
 import './LoginFormStyles.css';
+import AppInput from '../../components/AppInput/AppInput.jsx';
+import AppButton from '../../components/AppButton.jsx';
 
 function LoginForm() {
-	return <section className="LoginForm"></section>;
+	return (
+		<section className="LoginForm">
+			<div className="app-form-parent">
+				<div className="heading">
+					<div className="company-branding">
+						<img src="" alt="" />
+						<h3>COMPANY_NAME</h3>
+					</div>
+
+					<h2>Login to your account</h2>
+				</div>
+
+				<form action="">
+					<AppInput labelText="Username" />
+					<AppInput labelText="Password" />
+
+					<AppButton className="rounded-md">Login</AppButton>
+				</form>
+			</div>
+
+			<img src="" alt="" />
+		</section>
+	);
 }
 
 export default LoginForm;
