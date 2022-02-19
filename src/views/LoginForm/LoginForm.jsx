e735f69152aa25ae25e2202c2e615d916d01393c
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-19 10:55:55 +0600

    changed the login img to be a background of a div instead of an img tag

--- src/views/LoginForm/LoginForm.jsx
+++ src/views/LoginForm/LoginForm.jsx
@@ -1,12 +1,17 @@
 import './LoginFormStyles.css';
 import AppInput from '../../components/AppInput/AppInput.jsx';
 import AppButton from '../../components/AppButton.jsx';
+import loginFormImg from './login-bg.jpeg';
 
 function LoginForm() {
 	return (
 		<section className="LoginForm h-screen w-screen bg-gray-100">
 			<div className="page-hero">
-				<img src="login-bg.jpeg" alt="" className="shadow brightness-50" />
+				<div
+					className="img w-full h-64 bg-cover brightness-75"
+					style={{ backgroundImage: `url(${loginFormImg})` }}
+				></div>
+				{/* <img src="login-bg.jpeg" alt="" className="shadow brightness-50" /> */}
 				<div className="branding bg-white w-9/12 py-5 mx-auto rounded-xl shadow flex items-center justify-center gap-3 relative bottom-10">
 					<img
 						src="logos_temp/Logo.png"
@@ -14,7 +19,7 @@ function LoginForm() {
 						className="h-9 w-9"
 					/>
 					<h3 className="text-slate-700 text-xl">
-						Classic IT & Sky Mart Ltd.
+						Classic IT ERP Software
 					</h3>
 				</div>
 			</div>
@@ -23,11 +28,11 @@ function LoginForm() {
 				action=""
 				className="w-9/12 mt-12 mx-auto flex flex-col justify-between text-center gap-y-12"
 			>
-				<h1 className="text-lg text-gray-700">Login to your account</h1>
+				<h1 className="text-xl text-gray-700">Login to your account</h1>
 				<AppInput placeholder="Username"></AppInput>
 				<AppInput placeholder="Password"></AppInput>
 
-				<AppButton>
+				<AppButton className="transition-none">
 					<span className="font-bold">Login</span>
 				</AppButton>
 			</form>
