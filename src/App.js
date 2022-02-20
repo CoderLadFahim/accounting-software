c186a98b4f0f21e841e61417967daa79029b088e
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-20 15:54:37 +0600

    Added a userAuthenticated state to app and passing down its setter function to LoginForm

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import LoginForm from './views/LoginForm/LoginForm.jsx';
 
 function App() {
+	const [userIsAuthenticated, setUserAuthenticated] = useState(false);
+
 	return (
 		<div className="App bg-gray-50 font-roboto">
-			<LoginForm />
-			{/* <header className="Aap-header"> */}
-			{/* 	<AppNav /> */}
-			{/* 	<Table */}
-			{/* 		data={[ */}
-			{/* 			{ */}
-			{/* 				serial: 0, */}
-			{/* 				date: '14-2-2002', */}
-			{/* 				name: 'fahim', */}
-			{/* 				balance: 10000, */}
-			{/* 			}, */}
-			{/* 			{ */}
-			{/* 				serial: 1, */}
-			{/* 				date: '13-2-2002', */}
-			{/* 				name: 'Nazmul', */}
-			{/* 				balance: 14000, */}
-			{/* 			}, */}
-			{/* 			{ */}
-			{/* 				serial: 2, */}
-			{/* 				date: '16-2-2002', */}
-			{/* 				name: 'Nishat', */}
-			{/* 				balance: 12000, */}
-			{/* 			}, */}
-			{/* 		]} */}
-			{/* 	/> */}
-			{/* </header> */}
+			<LoginForm userAuthenticator={() => setUserAuthenticated(true)} />
 		</div>
 	);
 }
