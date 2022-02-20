c68c8834b2eaf54f78bc9736619ac7701e4877cb
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-20 17:50:07 +0600

    Registered authSlice as a slice in store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
+
 import './App.css';
 import LoginForm from './views/LoginForm/LoginForm.jsx';
 
 function App() {
-	const [userIsAuthenticated, setUserAuthenticated] = useState(false);
+	const userIsAuthenticated = useSelector(
+		(state) => state.auth.userIsAuthenticated
+	);
 
 	return (
 		<div className="App bg-gray-50 font-roboto">
-			<LoginForm userAuthenticator={() => setUserAuthenticated(true)} />
+			<LoginForm />
 		</div>
 	);
 }
--- src/features/Auth/authSlice.js
+++ src/features/Auth/authSlice.js
@@ -1,18 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
 
 const initialState = {
 	userIsAuthenticated: false,
 };
-
-export const incrementAsync = createAsyncThunk(
-	'counter/fetchCount',
-	async (amount) => {
-		const response = await fetchCount(amount);
-		// The value we return becomes the `fulfilled` action payload
-		return response.data;
-	}
-);
 
 export const authSlice = createSlice({
 	name: 'auth',
@@ -26,26 +16,17 @@ export const authSlice = createSlice({
 		},
 	},
 	extraReducers: (builder) => {
-		builder
-			.addCase(incrementAsync.pending, (state) => {
-				state.status = 'loading';
-			})
-			.addCase(incrementAsync.fulfilled, (state, action) => {
-				state.status = 'idle';
-				state.value += action.payload;
-			});
+		// builder
+		// 	.addCase(incrementAsync.pending, (state) => {
+		// 		state.status = 'loading';
+		// 	})
+		// 	.addCase(incrementAsync.fulfilled, (state, action) => {
+		// 		state.status = 'idle';
+		// 		state.value += action.payload;
+		// 	});
 	},
 });
 
 export const { login, logout } = authSlice.actions;
-
-export const selectCount = (state) => state.counter.value;
-
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-	const currentValue = selectCount(getState());
-	if (currentValue % 2 === 1) {
-		dispatch(incrementByAmount(amount));
-	}
-};
 
 export default authSlice.reducer;
