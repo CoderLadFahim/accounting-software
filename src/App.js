a615e4b7d868265a299135e68ae83561d248e91d
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-20 18:20:35 +0600

    LoginForm functional

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectUserIsAuthenticated } from './features/auth/authSlice.js';
-import { useState } from 'react';
-
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserIsAuthenticated } from './features/auth/authSlice.js';
+
+import AppNav from './components/AppNav/AppNav.jsx';
+import SideBar from './components/SideBar/SideBar.jsx';
 import LoginForm from './views/LoginForm/LoginForm.jsx';
 
 function App() {
 	const userIsAuthenticated = useSelector(selectUserIsAuthenticated);
 
 	return (
-		<div className="App bg-gray-50 font-roboto">
-			<LoginForm />
-		</div>
+		<Router>
+			<div className="App bg-gray-50 font-roboto">
+				{!userIsAuthenticated ? (
+					<LoginForm />
+				) : (
+					<>
+						<AppNav />
+						<SideBar />
+
+						<Routes>{/* routes go here */}</Routes>
+					</>
+				)}
+			</div>
+		</Router>
 	);
 }
 
