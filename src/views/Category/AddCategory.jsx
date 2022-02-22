10d95dd1c625df0f5426d97637f3bb696f5d9867
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-22 11:23:14 +0600

    Styled AddCategory for mobile screens

--- src/views/Category/AddCategory.jsx
+++ src/views/Category/AddCategory.jsx
@@ -1,30 +1,35 @@
 import './AddCategoryStyles.css';
 import AppInput from '../../components/AppInput/AppInput.jsx';
+import AppButton from '../../components/AppButton.jsx';
 
 function AddCategory() {
 	return (
-		<form action="" className="w-9/12 mx-auto space-y-10">
-			<h1 className="form-title">Add Category</h1>
+		<form action="" className="w-9/12 mx-auto space-y-10 -mt-3">
+			<h1 className="form-title text-2xl text-gray-700 pt-4 pb-2 border-b">Add Category</h1>
 
-			<div className="form-fields space-y-16">
+			<div className="form-fields space-y-10">
 				<div className="form-field">
 					<label htmlFor="category-name">Category Name*:</label>
 					<AppInput type="text" id="category-name" required />
 				</div>
 
 				<div className="form-field">
+					<label htmlFor="category-description">Category Description:</label>
 					<textarea
-						placeholder="Category description"
-						id=""
+						id="category-description"
 						name=""
 						className="w-full bg-white h-60 rounded-lg shadow p-3"
 					></textarea>
 				</div>
 
 				<div className="form-field">
-					<label htmlFor="img-upload">Add a category image</label>
-					<AppInput type="file" />
+					<label htmlFor="img-upload">Add a category image:</label>
+					<AppInput type="file" className="border-none" />
 				</div>
+
+				<AppButton>
+					Save Category
+				</AppButton>
 			</div>
 		</form>
 	);
