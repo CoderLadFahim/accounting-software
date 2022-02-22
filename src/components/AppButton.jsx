5b081da7e53621306e8ac7aba507679c0900ff63
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-22 17:15:49 +0600

    Designed AddCategory for mobile and tablet screens

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -1,6 +1,6 @@
 const AppButton = ({ children, className, ...props }) => (
 	<button
-		className={`AppButton bg-teal-600 px-6 transform transition hover:bg-teal-500
+		className={`AppButton bg-teal-600 hover:bg-teal-500 px-6 transform transition hover:bg-teal-500
 		py-2 rounded-lg drop-shadow-md text-white disabled:pointer-events-none disabled:opacity-50 ${className}`}
 		{...props}
 	>
--- src/views/Category/AddCategory.jsx
+++ src/views/Category/AddCategory.jsx
@@ -4,12 +4,15 @@ import AppButton from '../../components/AppButton.jsx';
 
 function AddCategory() {
 	return (
-		<form action="" className="w-9/12 mx-auto space-y-10 sm:w-7/12 md:w-6/12">
-			<h1 className="form-title text-2xl text-gray-700 pt-4 pb-2 border-b-2 border-slate-200">
+		<form
+			action=""
+			className="w-9/12 mx-auto space-y-10 sm:mt-20 sm:w-7/12 md:w-6/12 lg:w-4/12"
+		>
+			<h1 className="form-title text-2xl text-gray-700 pt-4 pb-2 border-b-2 border-slate-200 sm:mb-16 lg:mb-24">
 				Add Category
 			</h1>
 
-			<div className="form-fields space-y-10 sm:space-y-16 md:mt-16 md:space-y-20">
+			<div className="form-fields space-y-10 sm:space-y-16 md:mt-16 md:space-y-20 lg:space-y-24">
 				<div className="form-field">
 					<label htmlFor="category-name">Category Name*:</label>
 					<AppInput type="text" id="category-name" required />
