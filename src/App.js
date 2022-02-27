42e41f9204a0b988b41661a68973897b0b3c80ef
CoderLadFahim <fahimalemroz@gmail.com>
2022-02-27 11:43:19 +0600

    Added a Sidebar toggler for mobile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectUserIsAuthenticated } from './features/Auth/authSlice.js';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 import AppNav from './components/AppNav/AppNav.jsx';
 import SideBar from './components/SideBar/SideBar.jsx';
 import LoginForm from './views/LoginForm/LoginForm.jsx';
@@ -21,6 +24,12 @@ function App() {
 				<div className="App mt-16">
 					<AppNav />
 					<SideBar />
+					<button className="w-10 h-10 rounded-full shadow xl:hidden bg-green-400 absolute bottom-5 right-5">
+						<FontAwesomeIcon
+							icon={faXmark}
+							className="text-white transform scale-125"
+						/>
+					</button>
 
 					<Routes>
 						<Route path="/add-category" element={<AddCategory />} />
